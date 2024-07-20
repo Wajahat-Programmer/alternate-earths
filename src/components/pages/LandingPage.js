9a3d73f430b3b5318f331969e24780b76c4497be
Wajahat <wajahatalikhanundoscore@gmail.com>
2024-07-20 20:50:23 +0500

    Initial commit

---
+++ src/components/pages/LandingPage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../Navbar'
+
+
+
+function LandingPage() {
+  return (
+    <>
+    <Navbar className="top-1" />
+    </>
+  )
+}
+
+export default LandingPage
